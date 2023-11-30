ee07ef36c1c256c78ff4d16512faa2bbb2fc100f
Doro8811 <148296714+Doro8811@users.noreply.github.com>
2023-11-29 20:30:56 -0600

    Add files via upload
    Lab 13 materials
    Python Code + 3 screenshots

---
+++ CS101L Lab13/Lab13.py
@@ -0,0 +1,75 @@
+import turtle
+
+class Point(object):
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.x, self.y)
+        turtle.pendown()
+        turtle.color(self.color)
+        turtle.setheading(0)
+        self.draw_action()
+
+    def draw_action(self):
+        turtle.dot()
+
+class Box(Point):
+
+    def __init__(self, x1, y1, width, height, color):
+        super().__init__(x1, y1, color)
+        self.width = width
+        self.height = height
+
+    def draw_action(self):
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+        turtle.right(90)
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+
+class BoxFilled(Box):
+
+    def __init__(self, x1, y1, width, height, color, fillcolor):
+        super().__init__(x1, y1, width, height, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        '''Box.draw_action(self)'''
+        super().draw_action()
+        turtle.end_fill()
+
+class Circle(Point):
+
+    def __init__(self, x1, y1, radius, color):
+        super().__init__(x1, y1, color)
+        self.radius = radius
+
+    def draw_action(self):
+        turtle.circle(self.radius)
+
+class CircleFilled(Circle):
+
+    def __init__(self, x1, y1, radius, color, fillcolor):
+        super().__init__(x1, y1, radius, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        turtle.circle(self.radius)
+        turtle.end_fill()
+        
+if __name__ == "__main__":
+    b = BoxFilled(1, 2, 100, 200, "red", "Blue")
+    b.draw()
+    c = CircleFilled(1, 2, 200, "red", "blue")
+    c.draw()
